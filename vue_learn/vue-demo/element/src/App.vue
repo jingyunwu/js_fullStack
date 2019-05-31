450e9033d529f363571bc4856cbd3a20adca501d
jingyunwu <1029595155@qq.com>
2019-05-31 14:20:24 +0800

    update element

--- vue_learn/vue-demo/element/src/App.vue
+++ vue_learn/vue-demo/element/src/App.vue
@@ -21,6 +21,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 150px;
+  /* margin-top: 150px; */
 }
 </style>
--- vue_learn/vue-demo/element/src/components/Login.vue
+++ vue_learn/vue-demo/element/src/components/Login.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="login">
-  <div class="container">
-     <p class="title">后台管理系统</p>
-     <div class="msg mt20">
-       <el-input v-model="user" placeholder="请输入用户名"></el-input>
-     </div>
-     <div class="msg mt20">
-       <el-input v-model="password" placeholder="请输入密码"></el-input>
-     </div>
-     <div class="btn mt20">
-       <el-button type="primary" @click="login">登录</el-button>
-     </div>
-  </div>
+    <div class="container">
+      <p class="title">后台管理系统</p>
+      <div class="msg mt20">
+        <el-input v-model="user" placeholder="请输入用户名"></el-input>
+      </div>
+      <div class="msg mt20">
+        <el-input v-model="password" placeholder="请输入密码"></el-input>
+      </div>
+      <div class="btn mt20">
+        <el-button type="primary" @click="login">登录</el-button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -28,6 +28,12 @@ export default {
     login () {
       if (this.user && this.password) {
         localStorage.setItem('userName', this.user)
+        // 字符串 路由跳转
+        // this.$router.push('/main')
+        // 对象
+        // this.$router.push({ path: '/main' })
+        // 编程式跳转
+        this.$router.push({ name: 'Main', params: { userId: 1 } })
       }
     }
   }
@@ -42,7 +48,7 @@ export default {
   width: 300px;
   border-radius: 4px;
   margin: 0 auto;
-  box-shadow: 0 0 20px #ca6aca;
+  box-shadow: 0 0 20px #cac6c6;
   padding: 50px;
   margin-top: 200px;
 }
