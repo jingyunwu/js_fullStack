bff3dddf8cf67ed41e4c220caf8ead0a10530c9c
jingyunwu <1029595155@qq.com>
2019-06-10 22:37:12 +0800

    add 小米Lite

---
+++ wxapp/小米Lite/pages/index/index.js
@@ -0,0 +1,36 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    imgUrls: [
+      '../../images/Redmi.jpg',
+      '../../images/xiaomiAL.jpg',
+      '../../images/E55A.jpg'
+    ],
+    items: [
+      {
+        imgUrl: '',
+        channel: ''
+      },
+      {
+        imgUrl: '',
+        channel: ''
+      },
+      {
+        imgUrl: '',
+        channel: ''
+      },
+      {
+        imgUrl: '',
+        channel: ''
+      },
+
+    ]
+    
+  },
+  //事件处理函数
+ 
+
+})
