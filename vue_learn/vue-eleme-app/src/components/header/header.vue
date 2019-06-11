7f78f1f7ef093b99aa79ee6210531d244f3f3e1b
jingyunwu <1029595155@qq.com>
2019-06-11 23:19:29 +0800

    add header.vue

--- vue_learn/vue-eleme-app/src/components/header/header.vue
+++ vue_learn/vue-eleme-app/src/components/header/header.vue
@@ -2,7 +2,17 @@
   <div class="header">
     <div class="content-wrapper">
       <div class="avatar">
-        <img src="" alt="">
+        <img src="seller.avatar" alt="" width="64" height="64">
+      </div>
+      <div class="content">
+        <div class="title">
+          <span class="brand"></span>
+          <span class="name">{{seller.name}}</span>
+        </div>
+        <div class="description">{{seller.description}}/{{seller.deliveryTime}}分钟送达</div>
+        <div class="support" v-if="seller.supports">
+          <span class="icon" :class="classMap[seller.supports[0].type]"></span>
+        </div>
       </div>
     </div>
   </div>
