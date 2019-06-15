731ffc7046e84adb66dd24d2bad2ea9aff262c53
jingyunwu <1029595155@qq.com>
2019-06-15 22:46:50 +0800

    add vuex

---
+++ vue/vuex-counter/src/main.js
@@ -0,0 +1,10 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'; // mvvm
+import App from './App.vue'; // 根组件
+import store from './store'; // 统一管理应用状态
+// SPA
+new Vue({
+  el: '#app',
+  render: h => h(App),
+})
