d59100851f4c1d38cd2c9ff4e4d082d4e7f481ca
jingyunwu <1029595155@qq.com>
2019-06-16 14:01:40 +0800

    add vuex

---
+++ vue/do_vuex_counter/README.md
@@ -0,0 +1,9 @@
+- data 是组件自有数据，没有多少，思考会不会被共享，应用层共享的，只要多于一个组件要用，且关系不是简单的兄弟，就用vuex
+- 共享状态，
+  vuex state  管
+  mutations 才能修改
+  actions 被触发动作，提交mutations
+  getters 对 state的包装
+
+
+mapActions mapGetters 引入组件  
---
+++ vue/do_vuex_counter/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    Clicked {{$store.state.count}} times, count is {{evenOrOdd}}
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementIfOdd">increment if odd</button>
+    <button @click="incrementAsync">incrementAsync</button>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+  name: 'App',
+  components: {
+    
+  },
+  computed: mapGetters(
+    ['evenOrOdd']
+  ),
+  methods: mapActions (
+    [
+    'increment',
+    'decrement',
+    'incrementIfOdd',
+    'incrementAsync'
+    ]
+    
+  ),
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
