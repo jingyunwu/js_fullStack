4d2ddf5c332347326935d9569a77f0a764626672
jingyunwu <1029595155@qq.com>
2019-06-24 14:33:03 +0800

    add scroll

--- vue_learn/vue-music/src/components/pages/search.vue
+++ vue_learn/vue-music/src/components/pages/search.vue
@@ -1,15 +1,46 @@
 <template>
-  <div class="search-box-wrapper">
-    <!-- 搜索框 -->
-    <v-search-box></v-search-box>
+  <div class="search">
+    <div class="search-box-wrapper">
+      <!-- 搜索框 -->
+      <v-search-box></v-search-box>
+    </div>
+    <div class="shortcut-wrapper" ref="shortcutWrapper" v-show="!query">
+      <!-- 热门搜索 -->
+    <v-scroll class="shortcut" ref="shortcut" :data="shortcut" :refreshDelay="refreshDelay">
+      <div>
+        <div class="hot-key">
+          <h1 class="title">热门搜索</h1>
+          <ul>
+            <li class="item" v-for="(item, index) in hotKey" :key="index" @click="addQuery(item.first)">
+              <span>{{item.first}}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </v-scroll>
+    </div>
   </div>
 </template>
 
 <script>
 import searchBox from '../searchBox'
+import scroll from '../scroll'
 export default {
+  data () {
+    return {
+      query: false,
+      hotKey: [
+        {first: '许嵩新歌发送'},
+        {first: '许嵩新歌发送'},
+        {first: '许嵩新歌发送'},
+        {first: '五月天新歌发送'},
+        {first: '许嵩新歌发送'}
+      ]
+    }
+  },
   components: {
-    'v-search-box': searchBox
+    'v-search-box': searchBox,
+    'v-scroll': scroll
   }
 }
 </script>
