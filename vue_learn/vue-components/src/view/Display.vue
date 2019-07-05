e20a58b2db3ab48c5d61d8ecfb639d28ed40fe63
jingyunwu <1029595155@qq.com>
2019-07-05 17:57:09 +0800

    add 渲染

---
+++ vue_learn/vue-components/src/view/Display.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3>动态渲染 .vue 文件的组件--display</h3>
+    <i-display :code="code"></i-display>
+  </div>
+</template>
+
+<script>
+import iDisplay from '@/components/com/display.vue'
+import defaultCode from './default-code.js'
+export default {
+  components: {
+    iDisplay
+  },
+  data () {
+    return {
+      code: defaultCode
+    }
+    
+  } 
+}
+</script>
+
+<style>
+
+</style>
