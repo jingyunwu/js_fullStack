d1fd87030087116f6ed9bc654ae4644f12873caa
jingyunwu <1029595155@qq.com>
2019-08-05 22:08:48 +0800

    add index.js

--- interview-questions/question11/index.js
+++ interview-questions/question11/index.js
@@ -5,3 +5,19 @@ var b = 10;
 })();
 
 // 输出 [Function: b]
+
+// var a = ?
+// a 在什么情况下 会输出 "1"   "==" 的隐式转换 会用到 toString() 和  valueOf()
+var a = {
+  i: 1,
+  // toString() {
+  //   return a.i++;
+  // }
+  valueOf() {
+    return a.i++
+  }
+}
+
+if(a == 1 && a == 2 && a ==3) {
+  console.log(1);
+}
